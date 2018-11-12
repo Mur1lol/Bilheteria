1c8fdb1dc7e6707b8239c321fceb27493529879a
Murilo Brasil Cordeiro <bc.murilo.mbc@gmail.com>
2018-11-11 23:15:15 -0200

    cards

--- trabalho-final/resources/views/temporadas/index.blade.php
+++ trabalho-final/resources/views/temporadas/index.blade.php
@@ -3,11 +3,10 @@
 @section('conteudo')
 <div class="container">
     <h1>TEMPORADA</h1>
-    <div>
+    <div class="row">
         @foreach($temporadas as $temporada)
 
-
-            <div class="card text-center mb-3">
+            <!-- <div class="card text-center mb-3">
 				<div class="card-header">
 					{{ $temporada->descricao }}
 				</div>
@@ -19,16 +18,17 @@
 				<div class="card-footer text-muted">
 					{{ $temporada->created_at->diffForHumans() }}
 				</div>
-			</div>
+			</div> -->
 
-			<!-- <div class="card border-info mb-3" style="max-width: 18rem;">
-				<div class="card-header">{{ $temporada->descricao }}</div>
-				<div class="card-body">
-					<h5 class="card-title">{{ $temporada->descricao }}</h5>
-					<p class="card-text">{{ $temporada->inicio }}</p>
+
+				<div class="card border-info col-md-4" style="max-width: 18rem; margin: 5px;">
+					<div class="card-header">{{ $temporada->descricao }}</div>
+					<div class="card-body text-info">
+						<h5 class="card-title">{{ $temporada->inicio }}</h5>
+						<p class="card-text">{{ $temporada->created_at->diffForHumans() }}</p>
+						<a href="#" class="btn btn-primary">Go somewhere</a>
+					</div>
 				</div>
-			</div>
- -->
 
         @endforeach
 
